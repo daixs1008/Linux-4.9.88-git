1bd97a3e7378de61bf9dfddb63d037b6af0c1c0f
daixs1008 <394513180@qq.com>
2021-10-28 13:02:56 +0800

    1254

---
+++ myprase/professional linux kernel architecture.c
@@ -0,0 +1,63 @@
+Introduction 
+	Unix is simple and coherent,but it takes a genius
+	(or at any rate a programmer) to understand
+	and appreciate the simplicity.
+	--Dennis Ritchie
+	
+	Note grom the authors:Tes,we have lost our minds.
+	Be forewarned:you will lose yours too,
+	-- Benny Goodheart  James Cox
+	
+	Unix is distinguished by a simple,coherent,and lelgant design - truly remarkable features that have 
+	enabled the system to influence the world for more than a quarter of a century.And especially thanks
+	to the growing presence of Linux,the idea is still picking up momentum,which no end of the growth 
+	in sight.
+	Unix and Linux carry a certain fascination,and two quotations above hopefully capture the spirit of 
+	this attraction.Consider Dennis Ritchie`s quote:Is the coinventor of Unix at Bell Labs completely right
+	in saying that only a genius can appreciate the simplicity of Unix? Luckily not,because he puts himself
+	into perspective immediately by adding that programmers also qualify to value the essence of Unix.
+	Understanding the meagerly documented,demanding,and complex sources of Unix as of Linux 
+	is not alwalys an easy task.But once has atarted to experience the rich insights that can be gained from
+	the kernal sources,it is hard to escape the fascibation of Linux.It seems fair to warn you that it`s easy 
+	to get addicted to the joy of the operating system kernel once starting to dive into it.This was already 
+	noted by Benny Goodheart and James Cox,whose preface to their book the magic Garden Explained 
+	(SECOND QUOTATION ABOVE)explained the internals of Unix System V.And Linux is definitely also capable 
+	of helping you to lose your mind!
+	This book acts as a guide and companion that takes you through the kernel sources and sharpens your
+	awareness of the beauty,elegance,and --last but not least --esthetics of their concepts.There are,however,
+	some prerequisites to foster an understanding of the kernel.C should not just be a letter;neither
+	should it be a foreign language.Operating systems are supposed to be more than just a "Start"button,and 
+	a small amount of algorithmics can also do no harm.finally,it is preferable if computer architecture is not
+	just about how how to build the most fansy case.From an academic piont of view,this comes closest to the 
+	lectures "Systems Programming,""Algorithmics"and "Fundamentals of Operating Systems"The previous
+	edition of this book has been used to tesvh the fundamentals of Linux to advenced undergraduate
+	stufents in several universities,and I hope that the current edition will serve the same purpose.
+	Discussing all aforementioned in topics in details right now(or maybe you are not holding it,for this 
+	very reason),you`ll surely agree that this would not be a good ides.When a topic not directly related to 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
